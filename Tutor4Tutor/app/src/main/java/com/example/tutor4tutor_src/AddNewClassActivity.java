f459a78648dc336f76bd51c2c8c7c37383ad2fd3
jiyoonli <47153762+jiyoonli@users.noreply.github.com>
2020-03-12 03:05:01 -0400

    build an Add A New Class page #46 for #7

---
+++ Tutor4Tutor/app/src/main/java/com/example/tutor4tutor_src/AddNewClassActivity.java
@@ -0,0 +1,14 @@
+package com.example.tutor4tutor_src;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_class);
+    }
+}
---
+++ Tutor4Tutor/app/src/main/java/com/example/tutor4tutor_src/homepage.java
@@ -0,0 +1,41 @@
+package com.example.tutor4tutor_src;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class homepage extends AppCompatActivity {
+
+
+
+    EditText textView5, text;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+        button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homepage.this, login.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+}
+
+
+
+
+
+
+
