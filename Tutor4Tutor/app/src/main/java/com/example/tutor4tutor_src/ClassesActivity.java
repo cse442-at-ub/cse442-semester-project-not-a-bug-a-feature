abcaeb1b2460cf1496f657f493585e9db0fd15ac
jiyoonli <47153762+jiyoonli@users.noreply.github.com>
2020-03-26 21:23:35 +0900

    merge #46 #47 #48

--- Tutor4Tutor/app/src/main/java/com/example/tutor4tutor_src/ClassesActivity.java
+++ Tutor4Tutor/app/src/main/java/com/example/tutor4tutor_src/ClassesActivity.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,65 +24,15 @@ import java.util.List;
 
 public class ClassesActivity extends AppCompatActivity {
 
+    ArrayList<String> classdata = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Take a Lecture");
         setContentView(R.layout.activity_classes);
-
-        //this is temp content for post
-        Button button = (Button)findViewById(R.id.button2);
-        button.setText("Let's PHYSICS! \nPhysics \nEllen Yang ");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LecturesActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button = (Button)findViewById(R.id.button);
-        button.setText("You got Stat \nStatistics \nJenna Smith ");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LecturesActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button = (Button)findViewById(R.id.button7);
-        button.setText("Advanced Marketing \nBusiness \nLui Pearce ");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LecturesActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button = (Button)findViewById(R.id.button3);
-        button.setText("A Tutoring for Tech Report \nWriting \nEllen Yang ");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LecturesActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button = (Button)findViewById(R.id.button6);
-        button.setText("Physics Should Go on \nPhysics \nKate Taylor ");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LecturesActivity.class);
-                startActivity(intent);
-            }
-        });
-        //temp content for post END
-
-
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -88,8 +41,11 @@ public class ClassesActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Sprint2: Post New functionality will be added", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Sprint2: Post New functionality will be added", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent intent = new Intent(ClassesActivity.this, AddNewClassActivity.class);
+                intent.putExtra("classlist", classdata);
+                startActivityForResult(intent, 22);
             }
         });
 
@@ -100,8 +56,74 @@ public class ClassesActivity extends AppCompatActivity {
                 show();
             }
         });
+
+        setListView();
     }
 
+
+    public void setListView()
+    {
+        //example of one class
+        classdata.add("Let's Physics!" +"\n" + "PHY101");
+        classdata.add("EASY Statistics" +"\n" + "STA303");
+        classdata.add("Check Your Sentences" +"\n" + "WTR101");
+        classdata.add("Business Tutor" +"\n" + "Elementary Business");
+
+        listView = (ListView) findViewById(R.id.mList);
+
+        adapter = new ArrayAdapter<String>(ClassesActivity.this, android.R.layout.simple_list_item_1, classdata);
+        listView.setAdapter(adapter);
+
+        //Short Click for going to a detailed page (= a Lecture List Page)
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent openProfile = new Intent(ClassesActivity.this, LecturesActivity.class);
+                startActivity(openProfile);
+            }
+        });
+
+        //Long Click for DELETE a class
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, final View view, final int position, long id) {
+
+                AlertDialog.Builder dlg = new AlertDialog.Builder(view.getContext());
+                dlg.setTitle("Delete a Class")
+                        .setMessage("Are you sure to delete this class?")
+                        .setNegativeButton("Cancel",null)
+                        .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                //If click Delete button
+                                classdata.remove(position);
+                                adapter.notifyDataSetChanged();
+                                Snackbar.make(view,"Class is deleted.",2000).show();
+                            }
+                        })
+                        .show();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == 22)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                String text = data.getStringExtra("newclass");
+                classdata.add(text);
+                adapter.notifyDataSetChanged();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //START: a dialog for FILTER
     void show() {
         final List<String> ListItems = new ArrayList<>();
         ListItems.add("Physics");
@@ -147,6 +169,7 @@ public class ClassesActivity extends AppCompatActivity {
                 });
         builder.show();
     }
+    //END: a dialog for FILTER
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
