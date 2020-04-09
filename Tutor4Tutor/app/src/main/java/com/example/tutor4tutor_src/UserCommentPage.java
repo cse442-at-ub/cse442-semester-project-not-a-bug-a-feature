ee4828a5ebf89c431cd31f1558bfa55cea9adc61
yangwenxuan123 <43827893+yangwenxuan123@users.noreply.github.com>
2020-04-09 03:40:48 -0400

    added thumb up feature

--- Tutor4Tutor/app/src/main/java/com/example/tutor4tutor_src/UserCommentPage.java
+++ Tutor4Tutor/app/src/main/java/com/example/tutor4tutor_src/UserCommentPage.java
@@ -1,5 +1,6 @@
 package com.example.tutor4tutor_src;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,12 +21,22 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class UserCommentPage extends AppCompatActivity {
+    boolean isPressed = false;
+    String number ="";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commentpage);
+        //final boolean[] isPressed = new boolean[1];
+
+        //isPressed[0] = false;
         final TextView usernameview = (TextView)findViewById(R.id.commentpage_username);
+        final ImageButton likebtn = (ImageButton)findViewById(R.id.commentpage_likebtn);
+        final TextView numView = (TextView)findViewById(R.id.commentpage_likenumber);
         Button send = (Button)findViewById(R.id.commentpage_sendbtn);
         final EditText inputtext = (EditText) findViewById(R.id.editText13);
+        String likeclickedPath = "@drawable/icons8_facebook_like_100";
+        String unclickPath = "@drawable/icons8_facebook_like_100__1_";
+
         usernameview.setText(UserStates.INSTANCE.getSelectUsername());
         showComment();
 
@@ -35,6 +47,25 @@ public class UserCommentPage extends AppCompatActivity {
                 new ServerHelper().execute("set",inputtext.getText().toString());
                 finish();
                 startActivity(getIntent());
+            }
+        });
+
+        likebtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                int result = Integer.parseInt(numView.getText().toString());
+
+                if (!isPressed) {
+                    likebtn.setBackgroundResource(R.drawable.icons8_facebook_like_100__1_);
+                    result+=1;
+
+                }else{
+                    likebtn.setBackgroundResource(R.drawable.icons8_facebook_like_100);
+                    result-=1;
+                }
+                numView.setText(Integer.toString(result));
+                isPressed =!isPressed;
             }
         });
 
