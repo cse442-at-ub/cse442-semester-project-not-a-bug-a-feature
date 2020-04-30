a219ff18b1c021aa302ba7cac882f85e950fc96f
gunjangu <gunjangu@buffalo.edu>
2020-04-30 04:02:30 -0400

    #81 user gets a notification when someone replies to their post

--- Tutor4Tutor/app/src/main/java/com/example/tutor4tutor_src/ReplyPost.java
+++ Tutor4Tutor/app/src/main/java/com/example/tutor4tutor_src/ReplyPost.java
@@ -1,8 +1,13 @@
 package com.example.tutor4tutor_src;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +19,8 @@ import com.google.android.material.snackbar.Snackbar;
 public class ReplyPost extends AppCompatActivity {
 
     private static final String TAG = ReplyPost.class.getSimpleName();
+    private final String CHANNEL_ID = "reply_to_post";
+    private final int NOTIFICATION_ID = 001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +42,29 @@ public class ReplyPost extends AppCompatActivity {
         Snackbar.make(view, "Responnded to the post", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
 
+
+        CreateNotificationChannel();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_sms_notification);
+        builder.setContentTitle("testuser");
+        builder.setContentText("Message Send");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
+    }
+
+    private void CreateNotificationChannel(){
+        if(Build.VERSION.SDK_INT>Build.VERSION_CODES.O){
+            CharSequence name = "Tutor4Tutor";
+            String description = "Reply from a user";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,name,importance);
+            notificationChannel.setDescription(description);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 }
